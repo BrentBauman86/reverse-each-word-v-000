7b5cfc88eab0aa1828fbe09c6f215886368fc893
Brent Bauman <brentbauman86@gmail.com>
2018-04-19 17:06:23 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,16 @@
 
 def reverse_each_word(sentence)
   sentence_words = sentence.split(" ")
-  reversed_word = []
-    sentence_words.each do |word|
-      reversed_word.push(word.reverse)
+  #reversed_word = []
+    sentence_words.collect do |word|
+      word.reverse
+      #reversed_word.push(word.reverse)
 end
 
-      return reversed_word.join(" ")
+      #return reversed_word.join(" ")
 
-      sentence_words = sentence.split(" ")
-          sentence_words.collect do |word|
-              (word.reverse)
-      end
-end
+      #sentence_words = sentence.split(" ")
+          #sentence_words.collect do |word|
+              #(word.reverse)
+      #end
+#end
